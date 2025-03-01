3c8b9a84ae82b0840ece44da040f84e792a2b4a2
njonge9 <frednjonge9@gmail.com>
2025-03-02 01:33:52 +0300

    Validates the borrowing model

--- app/models/borrowing.rb
+++ app/models/borrowing.rb
@@ -1,4 +1,20 @@
 class Borrowing < ApplicationRecord
   belongs_to :user
   belongs_to :book
+
+  validates :due_date, presence: true
+  validates :book_availability, on: :create
+
+  before_create :set_due_date
+
+  def set_due_date
+    self.due_date = Date.today + 14.days
+    self.returned = false
+  end
+
+  def book_availability
+    unless book.available?
+      errors.add(:book, "is already borrowed")
+    end
+  end
 end
