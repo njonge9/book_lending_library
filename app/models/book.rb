ad10f22eca65f5e091e6e34c5640213ceb1a90fa
njonge9 <frednjonge9@gmail.com>
2025-03-02 01:29:28 +0300

    Validate the book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,12 @@
 class Book < ApplicationRecord
+    has_many :borrowings
+    has_many :users, through: :borrowings
+    validates :title, :author, :isbn, presence: true
+    validates :isbn, uniqueness: true
+    # validates :author, presence: true
+    # validates :isb, presence: true
+
+    def available?
+        borrowings.where(returned_at: false).empty?
+    end
 end
